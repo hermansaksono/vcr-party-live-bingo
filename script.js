833b22df79f08b7f016667c859f340dafdd63820
Herman Saksono <hermansaksono@Hermans-MacBook-Pro.local>
2021-04-28 12:48:15 -0400

    Clean up the script

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ Randomize array in-place using Durstenfeld shuffle algorithm. Adapted from:
 https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 */
 const shuffleArray = (array) => {
+	let outputArray = [...array]
+	
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        var temp = outputArray[i];
+        outputArray[i] = outputArray[j];
+        outputArray[j] = temp;
     }
+	return outputArray;
 }
 
 /*
-fillCells : void => void
-Randomize the entries array, then put the entries into the bingo card.
-INVARIANT: The bingoEntries array (see below) must be at least 24 entries.
+fillCells : Array => void
+Randomizes the given entriesArray, then put the entries into the bingo card.
+INVARIANT: The entriesArray array must be at least 24 entries.
 */
-const fillCells = () => {
-	shuffleArray(bingoEntries);
+const fillCells = (entriesArray) => {
+	let shuffledArray = shuffleArray(entriesArray);
 
 	for (i = 0; i < NUM_ENTRIES; i++) {
 		let td = document.querySelector(cells[i]);
-		td.innerHTML = bingoEntries[i];
+		td.innerHTML = shuffledArray[i];
 	}
+}
+
+/*
+init : void => void
+Initializes the Bingo card.
+INVARIANT: The bingoEntries array (in entries.js) must be at least 24 entries.
+*/
+const init = () => {
+	fillCells(bingoEntries);
 }
 
 
@@ -33,7 +45,7 @@ const fillCells = () => {
 let NUM_ENTRIES = 24;
 
 // DO NOT MODIFY. This is an array of predefined Bingo boxes ids.
-// Note that this 
+// Note that #cell33 is not included because it contains the star. 
 let cells = new Array(
 	"#cell11", "#cell12", "#cell13", "#cell14", "#cell15",
 	"#cell21", "#cell22", "#cell23", "#cell24", "#cell25",
@@ -43,4 +55,4 @@ let cells = new Array(
 );
 
 // RUN
-document.addEventListener("DOMContentLoaded", fillCells);
+document.addEventListener("DOMContentLoaded", init);
